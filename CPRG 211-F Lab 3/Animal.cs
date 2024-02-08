5d8d64e36470e652b720670defbdc280ebb38547
BrienMC <152676467+BrienMC@users.noreply.github.com>
2024-02-07 20:32:17 -0700

    Add files via upload

---
+++ CPRG 211-F Lab 3/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG_211_F_Lab_3
+{
+    public abstract class Animal
+    {
+
+        public string? Name { get; set; }
+        public string? Colour { get; set; }
+        public int Age { get; set; }
+        
+        public abstract void Eat();
+
+    }
+
+    public class Dog : Animal
+    {
+
+        public override void Eat()
+        {
+            Console.WriteLine("Dogs eats meat.");
+        }
+
+    }
+
+    public class Cat : Animal
+    {
+
+        public override void Eat()
+        {
+            Console.WriteLine("Cats eats mice.");
+        }
+
+    }
+
+}
---
+++ CPRG 211-F Lab 3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Data.Common;
+
+namespace CPRG_211_F_Lab_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+
+
+            //Dog Class
+            
+            Console.WriteLine("Enter dog name: ");
+
+            dog.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter dog color: ");
+
+            dog.Colour = Console.ReadLine();
+
+            Console.WriteLine("Enter dog age: ");
+
+            dog.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nThe dog's name is {dog.Name}, its colour is {dog.Colour} and is {dog.Age} years old.");
+            dog.Eat();
+            Console.WriteLine();
+
+
+
+            //Cat Class
+
+            Console.WriteLine("Enter cat name: ");
+
+            cat.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter cat color: ");
+
+            cat.Colour = Console.ReadLine();
+
+            Console.WriteLine("Enter cat age: ");
+
+            cat.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nThe cat's name is {cat.Name}, its colour is {cat.Colour} and is {cat.Age} years old.");
+            cat.Eat();
+            Console.WriteLine();
+
+        }
+    }
+}
